21b9f9b3b19db7a7a0bf6fe699f3baf4ab9d41d0
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-09-14 23:09:03 +0530

    modified:   pyids/pyids.py

--- pyids/pyids.py
+++ pyids/pyids.py
@@ -1,3 +1,11 @@
+from scapy.all import sniff, IP, TCP, UDP, Raw
+import argparse
+import logging
 
-
-
+# Setup logging to write alerts to a file and the console
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    handlers=[
+                        logging.FileHandler('alerts.log'),
+                        logging.StreamHandler()
+                    ])
