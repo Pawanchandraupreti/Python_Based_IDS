a444ca59794eb1e89bf3dd1746a64dac74174ab9
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-09-19 23:40:43 +0530

    modified:   pyids/pyids.py

--- pyids/pyids.py
+++ pyids/pyids.py
@@ -37,7 +37,6 @@ def load_rules(rule_file):
         logging.error(f"Rule file {rule_file} not found!")
     return rules_list
 
-
 def packet_callback(packet, rules):
     """Function called for every captured packet."""
     # Check if packet has an IP layer
